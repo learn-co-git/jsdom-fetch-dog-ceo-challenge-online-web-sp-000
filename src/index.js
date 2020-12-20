15a947109566c4a6672ebc325ee01ddfd161f96d
learn-co-git <argus.two.2@protonmail.com>
2020-12-19 23:06:04 -0700

    commit tree 888f6b3c3bc99b65d35813e699a3b7c540210ced

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+ document.addEventListener('DOMContentLoaded', (event) => {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    json.message.forEach(dog => getImage(dog));
+  });
+
+    function getImage(url) {
+      let li = document.createElement("IMG");
+      document.getElementById('dog-image-container').appendChild(li);
+      li.src = url;
+    };
+
+});
